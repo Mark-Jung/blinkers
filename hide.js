9fe2537e637371bd5aeb59ecaa353663c988d7c9
Mark Jung <gujung2022@u.northwestern.edu>
2017-09-11 01:29:51 +0900

    now nicely displays quotes

--- hide.js
+++ hide.js
@@ -5,8 +5,26 @@ var author = "";
 function replace() {
   var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
   var x = document.getElementsByClassName("_2pie");
-  // fetch_quote();
-  var html = `${quote} by ${author}`;
+  var html = `<div>
+                <p
+                  style="
+                    text-align:center;
+                    vertical-align:middle;
+                    text-shadow:2px 2px 3px rgba(150,150,150,0.75);
+                    font-size: 40px;
+                    padding:2% 15% 0 15%;
+                  ">
+                  ${quote}
+                </p>
+                <p
+                  style="
+                    text-align:center;
+                    text-shadow:2px 2px 3px rgba(150,150,150,0.75);
+                    font-size: 30px;
+                  ">
+                  -${author}
+                </p>
+              </div>`;
   x[0].innerHTML = html;
 
   // this also hides the friends activities
@@ -17,19 +35,6 @@ function replace() {
   // $('#pagelet_trending_tags_and_topics').remove();
   // $('#pagelet_canvas_nav_content').remove();
 }
-
-// function build_HTML(tag, html, attrs) {
-//   if (typeof(html) != 'string') {
-//     attrs = html;
-//     html = null;
-//   }
-//   var h = '<' + tag;
-//   for (attr in attrs) {
-//     if(attrs[attr] === false) continue;
-//     h += ' ' + attr + '="' + attrs[attr] + '"';
-//   }
-//   return h += html ? ">" + html + "</" + tag + ">" : "/>";
-// }
 
 function fetch_quote() {
   const url = "";
@@ -46,4 +51,4 @@ function fetch_quote() {
   });
 };
 fetch_quote();
-window.setInterval(replace, 100);
+window.setInterval(replace, 10);
